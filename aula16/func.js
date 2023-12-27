1a2f4a8735737d42d953c38eb93820b3051f2f89
AneilsonRocha <rochaaneilson445@gmail.com>
2023-12-26 22:17:34 -0300

    Função e exercicios

--- aula16/func.js
+++ aula16/func.js
@@ -1,3 +1,5 @@
+
+
 function parimp(num){
     if(num % 2 ==0){
         return 'par'
@@ -14,3 +16,18 @@ function soma (n1=1,n2=0) {
 }
 
 console.log(soma(7,9))
+
+
+let v =  function(n) {
+    return n*2
+}
+
+function fatorial(n){
+    let fat= 1
+    for(let c = n; c > 1;c--){
+        fat *= c
+    }
+    return fat
+}
+
+console.log(fatorial(5))
